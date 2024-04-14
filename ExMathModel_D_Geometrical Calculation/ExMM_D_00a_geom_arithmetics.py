f1fe2c9c4f5c1a46b5049fbb43677426d49b3faa
GMPythonGit <k-miura@bd5.so-net.ne.jp>
2024-04-15 08:51:29 +0900

    add and change from Home

---
+++ ExMathModel_D_Geometrical Calculation/ExMM_D_00a_geom_arithmetics.py
@@ -0,0 +1,34 @@
+## GMPython Exercises for Mathematical Modeling:: coded by Kinya MIURA
+## ExMM_B_00a_list_tuple_set.py:: append(), list(), tuple(), set()
+
+N = int(input())
+S = input()  # string
+
+strA = list(S.split())  # list of strings
+lstA = []  # list of integers
+for i in range(N):
+    lstA.append(int(strA[i]))
+print(f'{lstA = }')
+
+tplA = tuple(lstA)  # tuple of integers
+print(f'{tplA = }')
+
+setA = set(lstA)  # set of integers
+print(f'{setA = }')
+
+# =========================================================
+# *** list of input lines ***
+'''
+N
+A1, A2, ... AN
+
+[Case a]
+10
+8 4 9 5 3 6 0 2 7 1 
+
+[Case b]
+10
+5 2 0 4 3 5 1 0 5 2
+
+'''
+
